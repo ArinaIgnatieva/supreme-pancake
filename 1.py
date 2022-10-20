f557ca164403c45bf87265d5b55a147679e61de2
ArinaIgnatieva <arinaign04@mail.ru>
2022-10-20 13:51:56 +0300

    second

---
+++ 1.py
@@ -0,0 +1,18 @@
+def translation():
+    figure = int(input("Введите число:"))
+    system = int(input("Введите целевую систему счисления (2 или 8):"))
+    result = ''
+    if system == 2:
+        while figure > 0:
+            result = str(figure % 2) + result
+            figure = figure // 2 
+        print(result)
+    elif system == 8:
+        while figure > 0:
+            result = str(figure % 8) + result 
+            figure = figure // 8
+        print(result)
+    else:
+        print("Ошибка: введите одно из предложенных оснований систем счисления")
+
+translation()
